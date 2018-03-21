6b3e70951f710a39890ac87eeb14e1091fa2d4c2
15EC27 <namimadhan@gmail.com>
2018-03-21 16:31:05 +0530

    Create codeketa sub of 2 time.py

---
+++ Beginner level/codeketa sub of 2 time.py
@@ -0,0 +1,7 @@
+hour1 = float(input("Input the hour of first time: "))
+min1 = float(input("Input the minutes of first time: "))
+hour2 = float(input("Input the hour of second time: "))
+min2 = float(input("Input the minutes of second time: "))
+hour3 = hour1-hour2
+min3 = min1-min2
+print("h:m-> %d %d" % (hour3, min3))
